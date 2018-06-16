58fdce5c79d477fa307a8144cd7b020da5906f8d
SkyNet <nme312.noreply.github.com>
2018-06-15 22:48:26 -0500

    begin page formating

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <h1>Clicky Game</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12 header-col bg-primary">
+            <div className="page-header">
+              <div className="title-div col-md-4">
+                <strong>Clicky Game</strong>
+              </div>
+              <div className="game-info-div col-md-6">
+                Placeholde
+                Score: | Top Score:
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
