1d61834327ca717959bcfd87d2f1fe774a1ac3c3
SkyNet <nme312.noreply.github.com>
2018-06-16 22:55:23 -0500

    redesign App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react'
 import './App.css'
-import Card from "./components/FriendCard"
+import Card from "./components/Card"
 import Wrapper from "./components/Wrapper"
 import Title from "./components/Title"
+import cards from "./cards.json"
 
 class App extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <div className="row row-1">
-          <div className="col-md-8 offset-1 name-plate-col">
-            <a className="">Nicholas Edwards</a>
-          </div>
-          <div className="col-md-3 navbar-col">
-            <div className="nav-buttons">
-              <span className="col-md-6" id="guessStatus">(Guess Status{})</span>
-              <span className="col-md-3">Score:{} | Top Score:{}</span>
-            </div>
-          </div>
-        </div>
-
-
-      </div>
+      <Wrapper>
+        <Title>Click Game</Title>
+        <Card
+          id={cards.id}
+          image={cards.image}
+        />
+      </Wrapper>
     )
   }
 }
