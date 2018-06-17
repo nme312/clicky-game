d23eee8ceff3a8457a6782844a6b1f6df77c1fe6
SkyNet <nme312.noreply.github.com>
2018-06-17 11:59:09 -0500

    App.js cards map

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react'
-import './App.css'
 import Card from "./components/Card"
 import Wrapper from "./components/Wrapper"
 import Title from "./components/Title"
 import cards from "./cards.json"
+import './App.css'
 
 class App extends Component {
+  state = {
+    cards
+  }
+
+  randomizer = () => {
+    console.log(this.state.cards.id)
+  }
+
   render() {
     return (
       <Wrapper>
         <Title>Click Game</Title>
-        <Card
-          id={cards.id}
-          image={cards.image}
-        />
+        {this.state.cards.map(card => (
+          <Card
+            id={card.id}
+            key={card.id}
+            image={card.image}
+            randomizer={this.randomizer}
+          />
+        ))}
       </Wrapper>
     )
   }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,7 @@ import React from "react"
 import "./Card.css"
 
 const Card = props => (
-    <div className="card" id={props.id} onClick={}>
+    <div className="card" id={props.id} onClick={() => props.randomizer()} >
         <div className="img-container">
             <img src={props.image} alt={props.name} />
         </div>
